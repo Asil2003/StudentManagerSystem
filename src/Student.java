9a891147a52cb3f373305a28c1186c01875c2794
Asil2003 <asilalabri03@gmail.com>
2025-07-23 08:51:47 +0400

    make instance variable and constructor

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,11 @@
 public class Student {
+    private String name;
+    private int id;
+    private double grade;
+    //add a constructor
+    public Student(String name, int id, double grade) {
+        this.name = name;
+        this.id = id;
+        this.grade = grade;
+    }
 }
