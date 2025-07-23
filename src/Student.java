e7208340880afa902074669874b8ee939b4de564
Asil2003 <asilalabri03@gmail.com>
2025-07-23 18:27:33 +0400

    Add getters

--- src/Student.java
+++ src/Student.java
@@ -2,10 +2,30 @@ public class Student {
     private String name;
     private int id;
     private double grade;
-    //add a constructor
+
     public Student(String name, int id, double grade) {
         this.name = name;
         this.id = id;
         this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    public void displayInfo() {
+        System.out.println("ID: " + id + ", Name: " + name + ", Grade: " + grade);
     }
 }
